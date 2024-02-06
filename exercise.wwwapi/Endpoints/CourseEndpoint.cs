6bf81a572bffae5535f114480741ba975f17b393
André Sturesson <andre.m.sturesson@gmail.com>
2024-02-06 11:27:18 +0100

    Core + Extensions

--- exercise.wwwapi/Endpoints/CourseEndpoint.cs
+++ exercise.wwwapi/Endpoints/CourseEndpoint.cs
@@ -2,6 +2,7 @@
 using exercise.wwwapi.DataTransferObjects;
 using exercise.wwwapi.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Query.ExpressionTranslators.Internal;
 
 namespace exercise.wwwapi.Endpoints
 {
@@ -12,16 +13,39 @@ namespace exercise.wwwapi.Endpoints
     {
         public static void CourseEndpointConfiguration(this WebApplication app)
         {
-            var students = app.MapGroup("courses");
-            students.MapGet("/", GetCourses);
+            var courses = app.MapGroup("courses");
+            courses.MapGet("/", GetCourses);
+            courses.MapPost("/", AddCourse);
+            courses.MapPut("/{id}", UpdateCourse);
+            courses.MapDelete("/{id}", DeleteCourse);
         }
 
         [ProducesResponseType(StatusCodes.Status200OK)]
         public static async Task<IResult> GetCourses(IRepository repository)
         {
             var results = await repository.GetCourses();
-            var payload = new Payload<IEnumerable<Course>>() { data = results };
-            return TypedResults.Ok(payload);
+            return TypedResults.Ok(CourseDTO.FromRepository(results));
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public static async Task<IResult> AddCourse(IRepository repository, Course course)
+        {
+            var addedCourse = await repository.AddCourse(course);
+            return TypedResults.Created("Created", new CourseDTO(addedCourse));
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public static async Task<IResult> UpdateCourse(IRepository repository, int id, Course course)
+        {
+            var updatedCourse = await repository.UpdateCourse(id, course);
+            return TypedResults.Ok(new CourseDTO(updatedCourse));
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public static async Task<IResult> DeleteCourse(IRepository repository, int id)
+        {
+            await repository.DeleteCourse(id);
+            return TypedResults.NoContent();
         }
     }
 }
