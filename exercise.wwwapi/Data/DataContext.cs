e24c049e2b14322d541c431388df523981faf9be
Elias <elias.experis@se.experis.com>
2024-02-07 11:39:31 +0100

    docker compose done

--- exercise.wwwapi/Data/DataContext.cs
+++ exercise.wwwapi/Data/DataContext.cs
@@ -6,13 +6,8 @@ namespace exercise.wwwapi.Data
 {
     public class DataContext : DbContext
     {
-        private string _connectionString;
-
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
-            this.Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
