a735db640bb6b1586dea82d91b4dee41b32d41ab
llllllll-l <vincent.werelius@gmail.com>
2024-02-07 10:56:47 +0100

    startng over

--- exercise.wwwapi/Data/DataContext.cs
+++ exercise.wwwapi/Data/DataContext.cs
@@ -7,18 +7,18 @@ namespace exercise.wwwapi.Data
     public class DataContext : DbContext
     {
 
-        //private string _connectionString;
+        private string _connectionString;
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            //var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            //_connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
-            //this.Database.EnsureCreated();
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
+            this.Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseNpgsql(_connectionString);
-            //optionsBuilder.LogTo(message => Debug.WriteLine(message));
+            optionsBuilder.UseNpgsql(_connectionString);
+            optionsBuilder.LogTo(message => Debug.WriteLine(message));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
