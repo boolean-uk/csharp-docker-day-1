d43f47d27d5dbc024a61e66555634aa46183ceaa
Victor <victor.adamson@se.experis.com>
2024-02-08 08:29:51 +0100

    Implemented docker compose database

--- exercise.wwwapi/Data/DataContext.cs
+++ exercise.wwwapi/Data/DataContext.cs
@@ -6,18 +6,18 @@ namespace exercise.wwwapi.Data
 {
     public class DataContext : DbContext
     {
-        private string _connectionString;
+        //private string _connectionString;
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
-            this.Database.EnsureCreated();
+            // var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            // _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
+            // this.Database.EnsureCreated();
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql(_connectionString);
-            optionsBuilder.LogTo(message => Debug.WriteLine(message));
-        }
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseNpgsql(_connectionString);
+        //     optionsBuilder.LogTo(message => Debug.WriteLine(message));
+        // }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Student>().HasKey(x => new { x.Id });
