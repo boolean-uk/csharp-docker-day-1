d6a4e60c27b2142755ca5d57214e476a09b4de8f
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-12 15:58:38 +0100

    Added some seed data

---
+++ exercise.wwwapi/Data/SeedData/Seeder.cs
@@ -0,0 +1,49 @@
+﻿using exercise.wwwapi.DataModels;
+
+namespace exercise.wwwapi.Data.SeedData
+{
+    public class Seeder
+    {
+        private List<Course> _courses { get; set; } = new List<Course>();
+        private List<Student> _students { get; set; } = new List<Student>();
+
+        public Seeder() 
+        {
+            Course course = new Course()
+            {
+                Id = 1,
+                CourseTitle = "Fullstack C#",
+                CourseStartDate = DateTime.SpecifyKind(new DateTime(2024,01,07), DateTimeKind.Utc),
+            };
+
+            Student student = new Student()
+            {
+                Id = 1,
+                FirstName = "Stian",
+                LastName = "Gaustad",
+                DateOfBirth = DateTime.SpecifyKind(new DateTime(1996, 08, 20), DateTimeKind.Utc),
+                CourseID = 1,
+                AverageGrade = 3,
+            };
+
+            Student student2 = new Student()
+            {
+                Id = 2,
+                FirstName = "Tølløv",
+                LastName = "Aresvik",
+                DateOfBirth = DateTime.SpecifyKind(new DateTime(1996, 04, 15), DateTimeKind.Utc),
+                CourseID = 1,
+                AverageGrade = 3,
+            };
+
+            _courses.Add(course);
+            _students.Add(student);
+            _students.Add(student2);
+
+
+        }
+
+        public List<Course> Courses { get { return _courses; } }
+        public List<Student> Students { get {  return _students; } }
+    }
+}
---
+++ exercise.wwwapi/Migrations/20240212141157_addSeedData.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace exercise.wwwapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "courses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    course_title = table.Column<string>(type: "text", nullable: false),
+                    course_start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_courses", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    date_of_birth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    course_id = table.Column<int>(type: "integer", nullable: false),
+                    average_grade = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_students_courses_course_id",
+                        column: x => x.course_id,
+                        principalTable: "courses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "courses",
+                columns: new[] { "id", "course_start_date", "course_title" },
+                values: new object[] { 1, new DateTime(2024, 1, 7, 0, 0, 0, 0, DateTimeKind.Utc), "Fullstack C#" });
+
+            migrationBuilder.InsertData(
+                table: "students",
+                columns: new[] { "id", "average_grade", "course_id", "date_of_birth", "first_name", "last_name" },
+                values: new object[,]
+                {
+                    { 1, 3.0, 1, new DateTime(1996, 8, 20, 0, 0, 0, 0, DateTimeKind.Utc), "Stian", "Gaustad" },
+                    { 2, 3.0, 1, new DateTime(1996, 4, 15, 0, 0, 0, 0, DateTimeKind.Utc), "Tølløv", "Aresvik" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_students_course_id",
+                table: "students",
+                column: "course_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "students");
+
+            migrationBuilder.DropTable(
+                name: "courses");
+        }
+    }
+}
