b99484fc12871dca4bf71551fac1c1e119cbc28f
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-12 15:59:13 +0100

    Changed the models

--- exercise.wwwapi/DataModels/Student.cs
+++ exercise.wwwapi/DataModels/Student.cs
@@ -1,7 +1,29 @@
-﻿namespace exercise.wwwapi.DataModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.DataModels
 {
+    [Table("students")]
     public class Student
     {
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("date_of_birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Column("course_id")]
+        [ForeignKey("CourseID")]
+        public int CourseID { get; set; }
+
+        public Course Course { get; set;}
+
+        [Column("average_grade")]
+        public double AverageGrade { get; set; }
     }
 }
