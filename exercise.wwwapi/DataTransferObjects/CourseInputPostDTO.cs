bb63b598979d2959258936b540dfad41b718f1e2
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-13 09:51:02 +0100

    Course Endpoint configured

---
+++ exercise.wwwapi/DataTransferObjects/CourseInputPostDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace exercise.wwwapi.DataTransferObjects
+{
+    public class CourseInputPostDTO
+    {
+        public string CourseTitle { get; set; }
+
+        public DateTime CourseStartDate { get; set; }
+    }
+}
---
+++ exercise.wwwapi/DataTransferObjects/CourseInputPutDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace exercise.wwwapi.DataTransferObjects
+{
+    public class CourseInputDTO
+    {
+        public string CourseTitle { get; set; }
+
+        public DateTime CourseStartDate { get; set; }
+    }
+}
