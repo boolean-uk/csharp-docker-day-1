a5580cdcaf85a59a59afc09f75d609de7dbb50d4
Dennis Rizah Nordvi Osmani <dennis.osmani@no.experis.com>
2024-09-16 18:07:38 +0200

    core and extension endpoints done, need to create docker

--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,5 +1,7 @@
 using exercise.wwwapi.Data;
+using exercise.wwwapi.DataModels;
 using exercise.wwwapi.Endpoints;
+using exercise.wwwapi.Repository;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
@@ -13,6 +15,8 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString"));
     opt.LogTo(message => Debug.WriteLine(message));
 });
+builder.Services.AddScoped<IRepository<Student>, Repository<Student>>();
+builder.Services.AddScoped<IRepository<Course>, Repository<Course>>();
     
 var app = builder.Build();
 
@@ -27,7 +31,7 @@ app.UseHttpsRedirection();
 
 app.StudentEndpointConfiguration(); //core
 app.CourseEndpointConfiguration(); //extension
-app.ApplyProjectMigrations();
+// app.ApplyProjectMigrations();
 
 app.Run();
 
