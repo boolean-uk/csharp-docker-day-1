8e969528e7340da8e47497fe2a14d74e613dbe1a
agrmet <agron-metaj@hotmail.com>
2024-09-16 11:39:09 +0200

    Migrated to database, setup dockerfile, defined models

--- exercise.wwwapi/DataModels/Course.cs
+++ exercise.wwwapi/DataModels/Course.cs
@@ -1,7 +1,15 @@
-﻿namespace exercise.wwwapi.DataModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.DataModels
 {
+    [Table("courses")]
     public class Course
     {
+        [Column("id")]
         public int Id { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("start_date")]
+        public DateTime StartDate { get; set; }
     }
 }
--- exercise.wwwapi/DataModels/Student.cs
+++ exercise.wwwapi/DataModels/Student.cs
@@ -1,7 +1,23 @@
-﻿namespace exercise.wwwapi.DataModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.DataModels
 {
+    [Table("students")]
     public class Student
     {
+        [Column("id")]
         public int Id { get; set; }
+        [Column("first_name")]
+        public string FirstName { get; set; }
+        [Column("last_name")]
+        public string LastName { get; set; }
+        [Column("date_of_birth")]
+        public DateTime DateOfBirth { get; set; }
+        [ForeignKey("Course")]
+        [Column("courseid")]
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+        [Column("average_grade")]
+        public decimal AverageGrade { get; set; }
     }
 }
