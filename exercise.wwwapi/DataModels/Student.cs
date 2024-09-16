55d3d7d1ead16f506015965eab548be2d1455565
Jonas <jonashalvorsen2013@live.no>
2024-09-16 14:58:34 +0200

    wip

--- exercise.wwwapi/DataModels/Student.cs
+++ exercise.wwwapi/DataModels/Student.cs
@@ -1,7 +1,26 @@
-﻿namespace exercise.wwwapi.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.DataModels
 {
+    [Table("student")]
     public class Student
     {
+        [Key]
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("firstName")]
+        public string FirstName { get; set; }
+
+        [Column("lastName")]
+        public string LastName { get; set; }
+
+        [Column("dob")]
+        public DateTime DateTime { get; set; }
+
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
     }
 }
