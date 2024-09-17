217b0043774975ac61358c7bf328165c519f7a6b
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-09-17 15:27:35 +0200

    unhealthy...

--- exercise.wwwapi/Endpoints/CourseEndpoint.cs
+++ exercise.wwwapi/Endpoints/CourseEndpoint.cs
@@ -50,6 +50,43 @@ namespace exercise.wwwapi.Endpoints
             var payload = new Payload<IEnumerable<CourseDTO>>() { Data = courseDTOs };
             return TypedResults.Ok(payload);
         }
-        
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public static async Task<IResult> GetStudent(IRepository<Course> repository, int id)
+        {
+            try
+            {
+                var course = await repository.GetById(id);
+                List<StudentDTO> studentDTOs = new List<StudentDTO>();
+                foreach (var student in course.Students)
+                {
+                    StudentDTO studentDTO = new StudentDTO()
+                    {
+                        CourseName = course.Name,
+                        FirstName = student.FirstName,
+                        LastName = student.LastName,
+                        DateOfBirth = student.DateOfBirth
+                    };
+                    studentDTOs.Add(studentDTO);
+                }
+
+                CourseDTO courseDTO = new CourseDTO()
+                {
+                    Name = course.Name,
+                    Students = studentDTOs
+                };
+
+                var payload = new Payload<CourseDTO>() { Data = courseDTO };
+                return TypedResults.Ok(payload);
+            }
+            catch (Exception ex)
+            {
+                return TypedResults.Problem(ex.Message);
+            }
+
+
+        }
+
     }
 }
