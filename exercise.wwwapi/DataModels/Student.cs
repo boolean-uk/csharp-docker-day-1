1013ae6cea5e1a7da729ea7e748635baed27e8a0
Victor <victor.chicinas@gmail.com>
2024-09-17 09:08:34 +0200

    Getting docker up

--- exercise.wwwapi/DataModels/Student.cs
+++ exercise.wwwapi/DataModels/Student.cs
@@ -1,7 +1,21 @@
-﻿namespace exercise.wwwapi.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.DataModels
 {
+    [Table("students")]
     public class Student
     {
+        [Key]
         public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DOB {  get; set; }
+        public string CourseTitle { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public int AvarageGrade { get; set; }
     }
 }
