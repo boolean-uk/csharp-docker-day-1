49a7877b0bdb676bb0e9c0094720fb78b44e31da
abdi <abdi060902@outlook.com>
2025-02-04 15:17:41 +0100

    done
    clear

--- exercise.wwwapi/Endpoints/CourseEndpoint.cs
+++ exercise.wwwapi/Endpoints/CourseEndpoint.cs
@@ -12,16 +12,61 @@ namespace exercise.wwwapi.Endpoints
     {
         public static void CourseEndpointConfiguration(this WebApplication app)
         {
-            var students = app.MapGroup("courses");
-            students.MapGet("/", GetCourses);
+            var courses = app.MapGroup("courses");
+            courses.MapGet("/", GetCourses);
+            courses.MapPost("/", AddCourse);
+            courses.MapDelete("/", DeleteCourse);
+            courses.MapPut("/", UpdateCourse);
+
+        }
+
+        private static async Task<IResult> UpdateCourse(IRepository repository, int id, string name)
+        {
+            var updated = await repository.UpdateCourse(id,name);
+            CourseDTO courseDTO = new CourseDTO();
+            courseDTO.Id = updated.Id;
+            courseDTO.Name = updated.Name;
+            return TypedResults.Ok(courseDTO);
+
+        }
+
+        private static async Task<IResult> DeleteCourse(IRepository repository,int id)
+        {
+            var deleted = await repository.DeleteCourse(id);
+            CourseDTO courseDTO = new CourseDTO();
+            courseDTO.Id = deleted.Id;
+            courseDTO.Name = deleted.Name;
+            return TypedResults.Ok(courseDTO);
+
+        }
+
+        private static async Task<IResult> AddCourse(IRepository repository, string name)
+        {
+            var course= await repository.AddCourse(name);
+
+            CourseDTO courseDTO=new CourseDTO();
+            courseDTO.Id=course.Id;
+            courseDTO.Name=course.Name;
+
+
+            return TypedResults.Ok(courseDTO);
+            
         }
 
         [ProducesResponseType(StatusCodes.Status200OK)]
         public static async Task<IResult> GetCourses(IRepository repository)
         {
             var results = await repository.GetCourses();
-            var payload = new Payload<IEnumerable<Course>>() { Data = results };
-            return TypedResults.Ok(payload);
+
+           
+            var courseDTOs = results.Select(c => new CourseDTO
+            {
+                Id = c.Id,
+                Name = c.Name
+            }).ToList();
+
+            return TypedResults.Ok(courseDTOs);
         }
+
     }
 }
--- exercise.wwwapi/Repository/Repository.cs
+++ exercise.wwwapi/Repository/Repository.cs
@@ -11,14 +11,71 @@ namespace exercise.wwwapi.Repository
         {
             _db = db;
         }
+
+        public async Task<Course> AddCourse(string name)
+        {
+           
+            Course course = new Course();
+            course.Name = name;
+            await _db.Courses.AddAsync(course);
+            await _db.SaveChangesAsync();
+            return course;
+
+
+        }
+
+        public async Task<Student> AddStudent(string name)
+        {
+            Student student = new Student();
+            student.Name = name;
+            await _db.Students.AddAsync(student);
+            await _db.SaveChangesAsync();
+            return student;
+        }
+
+        public async  Task<Course> DeleteCourse(int id)
+        {
+            var courses = await _db.Courses.FirstOrDefaultAsync(c => c.Id == id);
+            _db.Courses.Remove(courses);
+            await _db.SaveChangesAsync();
+            return courses;
+     
+        }
+
+        public async Task<Student> DeleteStudent(int id)
+        {
+            var students = await _db.Students.FirstOrDefaultAsync(c => c.Id == id);
+            _db.Students.Remove(students);
+            await _db.SaveChangesAsync();
+            return students;
+        }
+
         public async Task<IEnumerable<Course>> GetCourses()
         {
-            return await _db.Courses.ToListAsync();
+            return await _db.Courses.OrderBy(x => x.Id).ToListAsync();
         }
 
         public async Task<IEnumerable<Student>> GetStudents()
         {
-            return await _db.Students.ToListAsync();
+            return await _db.Students.OrderBy(x => x.Id).ToListAsync();
+        }
+
+        public async Task<Course> UpdateCourse(int id, string name)
+        {
+            var courses = await _db.Courses.FirstOrDefaultAsync(c => c.Id == id);
+            courses.Id = id;
+            courses.Name = name;
+            await _db.SaveChangesAsync();
+            return courses;
+        }
+
+        public async Task<Student> UpdateStudent(int id, string name)
+        {
+            var students = await _db.Students.FirstOrDefaultAsync(c => c.Id == id);
+            students.Id = id;
+            students.Name= name;
+            await _db.SaveChangesAsync();
+            return students;
         }
     }
 }
